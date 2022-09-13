24b2b3f61625eb70a1e271df3e1321e6a9010fcc
RaymondWHZ <wuhaozhen1106@gmail.com>
2022-09-13 01:04:23 -0500

    Update reservation presentation

--- components/reservations.tsx
+++ components/reservations.tsx
@@ -1,10 +1,18 @@
-import React from "react";
-import {Calendar, Result} from "antd";
-import {SmileOutlined} from "@ant-design/icons";
-import {ReservationSlot} from "../types/types";
-import moment from "moment";
+import React, {PropsWithChildren, useMemo, useRef, useState} from "react";
+import {Button, Calendar, Card, Checkbox, DatePicker, Divider, Form, Input, message, Modal, Result} from "antd";
+import {Kit, ReservationSlot} from "../types/types";
+import moment, {Moment} from "moment";
+import {createReservation} from "../util/services";
+import TextArea from "antd/lib/input/TextArea";
+import {PortableText} from "@portabletext/react";
 
-export const reservationsToDates = (reservations: ReservationSlot[]): Set<string> => {
+const SubTitle: React.FC<PropsWithChildren> = ({ children }) => (
+  <span style={{ fontSize: "15px", fontWeight: "bold", marginBottom: "12px" }}>
+    {children}
+  </span>
+)
+
+const reservationsToDates = (reservations: ReservationSlot[]): Set<string> => {
   const dates = new Set<string>()
   for (let r of reservations) {
     for (let d = moment(r.startDate); d.isSameOrBefore(r.endDate); d = d.add(1, 'd')) {
@@ -16,28 +24,180 @@ export const reservationsToDates = (reservations: ReservationSlot[]): Set<string
 
 interface ReservationDisplayProps {
   unavailableDates: Set<string>
+  selectedDate?: Moment
+  onChange?: (date: Moment) => void
 }
 
-export const ReservationDisplay: React.FC<ReservationDisplayProps> = ({ unavailableDates }) => {
-  if (unavailableDates.size == 0) {
-    return (
-      <Result
-        icon={<SmileOutlined />}
-        title="暂无不可用时段"
-        subTitle="所有时段均可预约"
-      />
-    )
-  }
+const ReservationDisplay: React.FC<ReservationDisplayProps> = ({ unavailableDates, selectedDate, onChange }) => {
   return (
     <>
-      <span style={{ fontSize: "15px", fontWeight: "bold", marginBottom: "12px" }}>
-        不可用时段：
-      </span>
+      <SubTitle>
+        可预约日期：
+      </SubTitle>
       <Calendar
-        value={moment()}  // disable changing date
         fullscreen={false}
         disabledDate={date => unavailableDates.has(date.format("YYYY-MM-DD"))}
+        value={selectedDate}
+        onChange={onChange}
       />
     </>
+  )
+}
+
+interface ContactModalProps {
+  borrowInfo: any
+  onClose: () => void
+}
+
+const ContactModal: React.FC<ContactModalProps> = ({ borrowInfo, onClose }) => {
+  const [form] = Form.useForm();
+  const [submitting, setSubmitting] = useState(false)
+  const [success, setSuccess] = useState(false)
+  const abortController = useRef<AbortController | null>(null)
+
+  const onSubmit = async (data: any) => {
+    setSubmitting(true)
+    abortController.current = new AbortController()
+    const result = await createReservation({
+      ...borrowInfo,
+      ...data
+    }, abortController.current)
+    abortController.current = null
+    setSubmitting(false)
+    if (result) {
+      setSuccess(true)
+    } else {
+      message.error("提交失败，请重试")
+    }
+  }
+
+  const onCloseCancel = () => {
+    abortController.current?.abort()
+    setSubmitting(false)
+    setSuccess(false)
+    form.resetFields()
+    onClose()
+  }
+
+  return (
+    <Modal
+      title="填写联系信息"
+      open={borrowInfo}
+      okText="确认提交申请"
+      cancelText="取消"
+      onOk={() => form.submit()}
+      onCancel={onCloseCancel}
+      confirmLoading={submitting}
+      footer={success ? <></> : undefined}
+    >
+      {success ?
+        <Result
+          status="success"
+          title="申请提交成功"
+          subTitle="你将会收到一封确认邮件，包含下一步申请步骤，请注意查收"
+          extra={[
+            <Button key={0} onClick={onCloseCancel}>关闭窗口</Button>
+          ]}
+        />
+        :
+        <Form form={form} onFinish={onSubmit} labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+          <Form.Item name="name" label="借用人姓名" rules={[{ required: true, message: '请输入借用人姓名' }]}>
+            <Input placeholder="姓名" />
+          </Form.Item>
+          <Form.Item name="email" label="借用人 UIUC 邮箱" rules={[{ required: true, message: '请输入借用人 UIUC 邮箱' }]}>
+            <Input placeholder="@illinois.edu" />
+          </Form.Item>
+          <Form.Item name="wechat" label="借用人微信号" rules={[{ required: true, message: '请输入借用人微信号' }]}>
+            <Input placeholder="微信号" />
+          </Form.Item>
+        </Form>
+      }
+    </Modal>
+  )
+}
+
+export interface ReservationCardProps {
+  kit: Kit
+  reservations: ReservationSlot[]
+  style?: React.CSSProperties
+}
+
+export const ReservationCard: React.FC<ReservationCardProps> = ({ kit, reservations, style }) => {
+  const [agreed, setAgreed] = useState(false)
+  const [borrowInfo, setBorrowInfo] = useState<any>(null)
+  const [form] = Form.useForm();
+  const [selectedDate, setSelectedDate] = useState<Moment | null>(moment())
+
+  const onSubmitBorrowInfo = (data: any) => {
+    setBorrowInfo({
+      ...data,
+      startDate: data.dates[0].format("YYYY-MM-DD"),
+      endDate: data.dates[1].format("YYYY-MM-DD"),
+      kitId: kit._id,
+      kitName: kit.name
+    })
+  }
+
+  const unavailableDates = useMemo(
+    () => reservations ? reservationsToDates(reservations) : new Set<string>(),
+    [reservations]
+  )
+
+  return (
+    <Card
+      loading={!kit}
+      style={style}
+    >
+      {kit &&
+        <Form
+          form={form}
+          style={{ display: "flex", flexDirection: "column" }}
+          onFinish={onSubmitBorrowInfo}
+        >
+          <ReservationDisplay
+            unavailableDates={unavailableDates}
+            selectedDate={selectedDate ?? undefined}
+            onChange={date => {
+              setSelectedDate(date)
+              form.setFieldValue("dates", [date, null])
+            }}
+          />
+          <Divider/>
+          <SubTitle>
+            发起借用申请：
+          </SubTitle>
+          <Form.Item name="dates" rules={[{ required: true, message: '请输入借用起止日期' }]}>
+            <DatePicker.RangePicker
+              style={{ width: "100%" }}
+              placeholder={["起始日期", "结束日期"]}
+              disabledDate={date => unavailableDates.has(date.format("YYYY-MM-DD"))}
+              onChange={dates => {dates && setSelectedDate(dates![0])}}
+            />
+          </Form.Item>
+          <Form.Item name="project" rules={[{ required: true, message: '请输入项目名称' }]}>
+            <Input placeholder="项目名称" />
+          </Form.Item>
+          <Form.Item name="usage" rules={[{ required: true, message: '请输入使用场景' }]}>
+            <TextArea placeholder="使用场景" />
+          </Form.Item>
+          <SubTitle>
+            借用规则：
+          </SubTitle>
+          <PortableText value={kit.rules}/>
+          <Checkbox style={{ marginBottom: "8px" }} checked={agreed} onChange={e => setAgreed(e.target.checked)}>
+            我已阅读并同意借用规则和<a href={"/"} target="_blank" rel="noreferrer">器材库使用须知</a>
+          </Checkbox>
+          <Form.Item style={{ width: "100%" }}>
+            <Button type="primary" htmlType="submit" style={{ width: "100%" }} disabled={!agreed}>
+              提交借用申请
+            </Button>
+          </Form.Item>
+          <span style={{ width: "100%", textAlign: "center", color: "gray", fontSize: "12px", marginTop: "-15px" }}>
+            请在下一步打开的弹窗中填写联系信息
+          </span>
+          <ContactModal borrowInfo={borrowInfo} onClose={() => setBorrowInfo(null)}/>
+        </Form>
+      }
+    </Card>
   )
 }
--- pages/kit/[id].tsx
+++ pages/kit/[id].tsx
@@ -1,21 +1,15 @@
 import { useRouter } from 'next/router'
 import {
-  Breadcrumb, Button,
-  Card,
-  Checkbox,
-  DatePicker,
-  Divider,
-  Form,
-  Input, message, Modal, Result
+  Breadcrumb,
+  Card
 } from "antd";
 import Link from "next/link";
-import {createReservation, useKitDetail} from "../../util/services";
-import React, {PropsWithChildren, useMemo, useRef, useState} from "react";
-import {Kit, ReservationSlot} from "../../types/types";
+import {useKitDetail} from "../../util/services";
+import React from "react";
+import {Kit} from "../../types/types";
 import {PortableText} from "@portabletext/react";
 import Meta from "antd/lib/card/Meta";
-import TextArea from "antd/lib/input/TextArea";
-import {ReservationDisplay, reservationsToDates} from "../../components/reservations";
+import {ReservationCard} from "../../components/reservations";
 import {KitImages} from "../../components/images";
 import {KitStatusTag, KitTypeTag} from "../../components/tags";
 import Head from "next/head";
@@ -46,140 +40,7 @@ const DescriptionCard: React.FC<{ kit: Kit }> = ({ kit }) => {
   )
 }
 
-const SubTitle: React.FC<PropsWithChildren> = ({ children }) => (
-  <span style={{ fontSize: "15px", fontWeight: "bold", marginBottom: "12px" }}>
-    {children}
-  </span>
-)
 
-const ContactModal: React.FC<{ borrowInfo: any, onClose: () => void }> = ({ borrowInfo, onClose }) => {
-  const [form] = Form.useForm();
-  const [submitting, setSubmitting] = useState(false)
-  const [success, setSuccess] = useState(false)
-  const abortController = useRef<AbortController | null>(null)
-
-  const onSubmit = async (data: any) => {
-    setSubmitting(true)
-    abortController.current = new AbortController()
-    const result = await createReservation({
-      ...borrowInfo,
-      ...data
-    }, abortController.current)
-    abortController.current = null
-    setSubmitting(false)
-    if (result) {
-      setSuccess(true)
-    } else {
-      message.error("提交失败，请重试")
-    }
-  }
-
-  const onCloseCancel = () => {
-    abortController.current?.abort()
-    setSubmitting(false)
-    setSuccess(false)
-    form.resetFields()
-    onClose()
-  }
-
-  return (
-    <Modal
-      title="填写联系信息"
-      open={borrowInfo}
-      okText="确认提交申请"
-      cancelText="取消"
-      onOk={() => form.submit()}
-      onCancel={onCloseCancel}
-      confirmLoading={submitting}
-      footer={success ? <></> : undefined}
-    >
-      {success ?
-        <Result
-          status="success"
-          title="申请提交成功"
-          subTitle="你将会收到一封确认邮件，包含下一步申请步骤，请注意查收"
-          extra={[
-            <Button key={0} onClick={onCloseCancel}>关闭窗口</Button>
-          ]}
-        />
-        :
-        <Form form={form} onFinish={onSubmit} labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
-          <Form.Item name="name" label="借用人姓名" rules={[{ required: true, message: '请输入借用人姓名' }]}>
-            <Input placeholder="姓名" />
-          </Form.Item>
-          <Form.Item name="email" label="借用人 UIUC 邮箱" rules={[{ required: true, message: '请输入借用人 UIUC 邮箱' }]}>
-            <Input placeholder="@illinois.edu" />
-          </Form.Item>
-          <Form.Item name="wechat" label="借用人微信号" rules={[{ required: true, message: '请输入借用人微信号' }]}>
-            <Input placeholder="微信号" />
-          </Form.Item>
-        </Form>
-      }
-    </Modal>
-  )
-}
-
-const BorrowCard: React.FC<{ kit: Kit, reservations: ReservationSlot[] }> = ({ kit, reservations }) => {
-  const [agreed, setAgreed] = useState(false)
-  const [borrowInfo, setBorrowInfo] = useState<any>(null)
-
-  const onSubmitBorrowInfo = (data: any) => {
-    setBorrowInfo({
-      ...data,
-      startDate: data.dates[0].format("YYYY-MM-DD"),
-      endDate: data.dates[1].format("YYYY-MM-DD"),
-      kitId: kit._id,
-      kitName: kit.name
-    })
-  }
-
-  const unavailableDates = useMemo(
-    () => reservations ? reservationsToDates(reservations) : new Set<string>(),
-    [reservations]
-  )
-
-  return (
-    <Card
-      loading={!kit}
-      style={{ width: "370px" }}
-    >
-      {kit &&
-        <Form style={{ display: "flex", flexDirection: "column" }} onFinish={onSubmitBorrowInfo}>
-          <ReservationDisplay unavailableDates={unavailableDates}/>
-          <Divider/>
-          <SubTitle>
-              发起借用申请：
-          </SubTitle>
-          <Form.Item name="dates" rules={[{ required: true, message: '请输入借用起止日期' }]}>
-              <DatePicker.RangePicker style={{ width: "100%" }} placeholder={["起始日期", "结束日期"]} disabledDate={date => unavailableDates.has(date.format("YYYY-MM-DD"))} />
-          </Form.Item>
-          <Form.Item name="project" rules={[{ required: true, message: '请输入项目名称' }]}>
-              <Input placeholder="项目名称" />
-          </Form.Item>
-          <Form.Item name="usage" rules={[{ required: true, message: '请输入使用场景' }]}>
-              <TextArea placeholder="使用场景" />
-          </Form.Item>
-          <SubTitle>
-              借用规则：
-          </SubTitle>
-          <PortableText value={kit.rules}/>
-          <Checkbox style={{ marginBottom: "8px" }} checked={agreed} onChange={e => setAgreed(e.target.checked)}>
-              我已阅读并同意借用规则和<a href={"/"} target="_blank" rel="noreferrer">器材库使用须知</a>
-          </Checkbox>
-          <Form.Item style={{ width: "100%" }}>
-              <Button type="primary" htmlType="submit" style={{ width: "100%" }} disabled={!agreed}>
-                  提交借用申请
-              </Button>
-          </Form.Item>
-          <span style={{ width: "100%", textAlign: "center", color: "gray", fontSize: "12px", marginTop: "-15px" }}>
-            请在下一步打开的弹窗中填写联系信息
-          </span>
-          <ContactModal borrowInfo={borrowInfo} onClose={() => setBorrowInfo(null)}/>
-        </Form>
-      }
-    </Card>
-  )
-}
 
 const KitDetail = () => {
   const router = useRouter()
@@ -207,7 +68,7 @@ const KitDetail = () => {
         </Breadcrumb>
         <div style={{ display: "flex" }}>
           <DescriptionCard kit={kit}/>
-          <BorrowCard kit={kit} reservations={reservations}/>
+          <ReservationCard kit={kit} reservations={reservations} style={{ width: "370px" }}/>
         </div>
       </div>
     </div>
