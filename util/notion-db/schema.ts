f36037337c0544547fa660266b4db1b4622fce86
RaymondWHZ <wuhaozhen1106@gmail.com>
2024-05-10 01:10:04 -0500

    feat: support all Notion types

--- util/notion-db/schema.ts
+++ util/notion-db/schema.ts
@@ -69,7 +69,7 @@ const createdByToName: NotionPropertyDefinition<'created_by', string> = {
 }
 const createdByOptions = {
   ...makeDefaultOptions('created_by'),
-  nameString: () => createdByToName
+  name: () => createdByToName
 }
 export function created_by() {
   return createdByOptions;
@@ -109,135 +109,17 @@ export function date() {
   return dateOptions;
 }
 
-const richTextToPlainText: NotionMutablePropertyDefinition<'rich_text', string> = {
-  type: 'rich_text',
-  handler: value => packPlainText(value.rich_text),
-  composer: (value) => [{ text: { content: value } }]
-}
-const richTextOptions = {
-  ...makeDefaultOptions('rich_text'),
-  plainText: () => richTextToPlainText
-}
-export function rich_text() {
-  return richTextOptions;
-}
-
-const titleToPlainText: NotionMutablePropertyDefinition<'title', string> = {
-  type: 'title',
-  handler: value => packPlainText(value.title),
-  composer: (value) => [{ text: { content: value } }]
-}
-const titleOptions = {
-  ...makeDefaultOptions('title'),
-  plainText: () => titleToPlainText
-}
-export function title() {
-  return titleOptions;
-}
-
-const relationToIds: NotionMutablePropertyDefinition<'relation', string[]> = {
-  type: 'relation',
-  handler: value => value.relation.map(relation => relation.id),
-  composer: (value) => value.map(id => ({ id }))
-}
-const relationToSingleId: NotionMutablePropertyDefinition<'relation', string> = {
-  type: 'relation',
-  handler: value => value.relation[0].id ?? '',
-  composer: (value) => [{ id: value }]
-}
-const relationOptions = {
-  ...makeDefaultOptions('relation'),
-  ids: () => relationToIds,
-  singleId: () => relationToSingleId
-}
-export function relation() {
-  return relationOptions;
-}
-
-const statusToNameString: NotionMutablePropertyDefinition<'status', string> = {
-  type: 'status',
-  handler: value => value.status?.name ?? '',
-  composer: (value) => ({ name: value })
-}
-const statusOptions = {
-  ...makeDefaultOptions('status'),
-  string: () => statusToNameString,
-  stringEnum: <T extends string>(values: readonly T[]): NotionPropertyDefinition<'status', T> => {
-    return {
-      type: 'status',
-      handler: (value: ValueType<'status'>): T => {
-        const name = value.status?.name;
-        if (!name || !values.includes(name as T)) {
-          throw Error('Invalid status: ' + name);
-        }
-        return name as T;
-      }
-    }
-  },
-}
-export function status() {
-  return statusOptions;
-}
-
-const numberToNumberDefaultZero: NotionMutablePropertyDefinition<'number', number> = {
-  type: 'number',
-  handler: value => value.number ?? 0,
+const emailToString: NotionMutablePropertyDefinition<'email', string> = {
+  type: 'email',
+  handler: value => value.email ?? '',
   composer: (value) => value
 }
-export function number() {
-  return {
-    ...makeDefaultOptions('number'),
-    numberDefaultZero: () => numberToNumberDefaultZero
-  }
+const emailOptions = {
+  ...makeDefaultOptions('email'),
+  string: () => emailToString
 }
-
-const formulaToString: NotionPropertyDefinition<'formula', string> = {
-  type: 'formula',
-  handler: value => {
-    if (value.formula.type === 'string') {
-      return value.formula.string ?? '';
-    } else if (value.formula.type === 'number') {
-      return value.formula.number?.toString() ?? '';
-    } else if (value.formula.type === 'boolean') {
-      return value.formula.boolean ? 'true' : 'false';
-    } else if (value.formula.type === 'date') {
-      return value.formula.date?.start ?? '';
-    }
-    return '';
-  }
-}
-const formulaToBooleanDefaultFalse: NotionPropertyDefinition<'formula', boolean> = {
-  type: 'formula',
-  handler: value => value.formula.type === 'boolean' ? value.formula.boolean ?? false : false
-}
-const formulaToNumberDefaultZero: NotionPropertyDefinition<'formula', number> = {
-  type: 'formula',
-  handler: value => value.formula.type === 'number' ? value.formula.number ?? 0 : 0
-}
-const formulaToDateRange: NotionPropertyDefinition<'formula', DateRange> = {
-  type: 'formula',
-  handler: value => {
-    if (value.formula.type === 'date') {
-      return {
-        start: value.formula.date?.start ?? '',
-        end: value.formula.date?.end ?? ''
-      }
-    }
-    return {
-      start: '',
-      end: ''
-    }
-  }
-}
-const formulaOptions = {
-  ...makeDefaultOptions('formula'),
-  string: () => formulaToString,
-  booleanDefaultFalse: () => formulaToBooleanDefaultFalse,
-  numberDefaultZero: () => formulaToNumberDefaultZero,
-  dateRange: () => formulaToDateRange
-}
-export function formula() {
-  return formulaOptions;
+export function email() {
+  return emailOptions;
 }
 
 const filesToUrls: NotionPropertyDefinition<'files', string[]> = {
@@ -307,44 +189,77 @@ export function files() {
   return filesOptions;
 }
 
-const selectToNameString: NotionMutablePropertyDefinition<'select', string> = {
-  type: 'select',
-  handler: value => value.select?.name ?? '',
-  composer: (value) => ({ name: value })
+const formulaToString: NotionPropertyDefinition<'formula', string> = {
+  type: 'formula',
+  handler: value => {
+    if (value.formula.type === 'string') {
+      return value.formula.string ?? '';
+    } else if (value.formula.type === 'number') {
+      return value.formula.number?.toString() ?? '';
+    } else if (value.formula.type === 'boolean') {
+      return value.formula.boolean ? 'true' : 'false';
+    } else if (value.formula.type === 'date') {
+      return value.formula.date?.start ?? '';
+    }
+    return '';
+  }
 }
-const selectOptions = {
-  ...makeDefaultOptions('select'),
-  string: () => selectToNameString,
-  stringEnum: <T extends string>(values: T[]): NotionPropertyDefinition<'select', T> => {
-    return {
-      type: 'select',
-      handler: (value: ValueType<'select'>): T => {
-        const name = value.select?.name;
-        if (!name || !values.includes(name as T)) {
-          throw Error('Invalid status');
-        }
-        return name as T;
+const formulaToBooleanDefaultFalse: NotionPropertyDefinition<'formula', boolean> = {
+  type: 'formula',
+  handler: value => value.formula.type === 'boolean' ? value.formula.boolean ?? false : false
+}
+const formulaToNumberDefaultZero: NotionPropertyDefinition<'formula', number> = {
+  type: 'formula',
+  handler: value => value.formula.type === 'number' ? value.formula.number ?? 0 : 0
+}
+const formulaToDateRange: NotionPropertyDefinition<'formula', DateRange> = {
+  type: 'formula',
+  handler: value => {
+    if (value.formula.type === 'date') {
+      return {
+        start: value.formula.date?.start ?? '',
+        end: value.formula.date?.end ?? ''
       }
     }
-  },
-  optionalStringEnum: <T extends string>(values: T[]): NotionPropertyDefinition<'status', T | undefined> => {
     return {
-      type: 'status',
-      handler: (value: ValueType<'status'>): T | undefined => {
-        const name = value.status?.name;
-        if (!name) {
-          return undefined;
-        }
-        if (!values.includes(name as T)) {
-          throw Error('Invalid status');
-        }
-        return name as T;
-      }
+      start: '',
+      end: ''
     }
   }
 }
-export function select() {
-  return selectOptions;
+const formulaOptions = {
+  ...makeDefaultOptions('formula'),
+  string: () => formulaToString,
+  booleanDefaultFalse: () => formulaToBooleanDefaultFalse,
+  numberDefaultZero: () => formulaToNumberDefaultZero,
+  dateRange: () => formulaToDateRange
+}
+export function formula() {
+  return formulaOptions;
+}
+
+const lastEditedByToName: NotionPropertyDefinition<'last_edited_by', string> = {
+  type: 'last_edited_by',
+  handler: value => 'name' in value.last_edited_by ? value.last_edited_by.name ?? '' : ''
+}
+const lastEditedByOptions = {
+  ...makeDefaultOptions('last_edited_by'),
+  name: () => lastEditedByToName
+}
+export function last_edited_by() {
+  return lastEditedByOptions;
+}
+
+const lastEditedTimeToString: NotionPropertyDefinition<'last_edited_time', string> = {
+  type: 'last_edited_time',
+  handler: value => value.last_edited_time
+}
+const lastEditedTimeOptions = {
+  ...makeDefaultOptions('last_edited_time'),
+  timeString: () => lastEditedTimeToString
+}
+export function last_edited_time() {
+  return lastEditedTimeOptions;
 }
 
 const multiSelectToNameStrings: NotionMutablePropertyDefinition<'multi_select', string[]> = {
@@ -372,17 +287,79 @@ export function multi_select() {
   return multiSelectOptions;
 }
 
-const emailToString: NotionMutablePropertyDefinition<'email', string> = {
-  type: 'email',
-  handler: value => value.email ?? '',
+const numberToNumberDefaultZero: NotionMutablePropertyDefinition<'number', number> = {
+  type: 'number',
+  handler: value => value.number ?? 0,
   composer: (value) => value
 }
-const emailOptions = {
-  ...makeDefaultOptions('email'),
-  string: () => emailToString
+export function number() {
+  return {
+    ...makeDefaultOptions('number'),
+    numberDefaultZero: () => numberToNumberDefaultZero
+  }
 }
-export function email() {
-  return emailOptions;
+
+const peopleToNames: NotionMutablePropertyDefinition<'people', string[]> = {
+  type: 'people',
+  handler: value => value.people.reduce((acc, person) => {
+    if ('name' in person) {
+      return acc.concat(person.name ?? '');
+    }
+    return acc;
+  }, [] as string[]),
+  composer: (value) => value.map(name => ({ name }))
+}
+const peopleOptions = {
+  ...makeDefaultOptions('people'),
+  names: () => peopleToNames
+}
+export function people() {
+  return peopleOptions;
+}
+
+const phoneNumberToString: NotionMutablePropertyDefinition<'phone_number', string> = {
+  type: 'phone_number',
+  handler: value => value.phone_number ?? '',
+  composer: (value) => value
+}
+const phoneNumberOptions = {
+  ...makeDefaultOptions('phone_number'),
+  string: () => phoneNumberToString
+}
+export function phone_number() {
+  return phoneNumberOptions;
+}
+
+const relationToIds: NotionMutablePropertyDefinition<'relation', string[]> = {
+  type: 'relation',
+  handler: value => value.relation.map(relation => relation.id),
+  composer: (value) => value.map(id => ({ id }))
+}
+const relationToSingleId: NotionMutablePropertyDefinition<'relation', string> = {
+  type: 'relation',
+  handler: value => value.relation[0].id ?? '',
+  composer: (value) => [{ id: value }]
+}
+const relationOptions = {
+  ...makeDefaultOptions('relation'),
+  ids: () => relationToIds,
+  singleId: () => relationToSingleId
+}
+export function relation() {
+  return relationOptions;
+}
+
+const richTextToPlainText: NotionMutablePropertyDefinition<'rich_text', string> = {
+  type: 'rich_text',
+  handler: value => packPlainText(value.rich_text),
+  composer: (value) => [{ text: { content: value } }]
+}
+const richTextOptions = {
+  ...makeDefaultOptions('rich_text'),
+  plainText: () => richTextToPlainText
+}
+export function rich_text() {
+  return richTextOptions;
 }
 
 const rollupToDateRange: NotionPropertyDefinition<'rollup', DateRange> = {
@@ -428,4 +405,95 @@ const rollupOptions = {
 }
 export function rollup() {
   return rollupOptions;
+}
+
+const selectToNameString: NotionMutablePropertyDefinition<'select', string> = {
+  type: 'select',
+  handler: value => value.select?.name ?? '',
+  composer: (value) => ({ name: value })
+}
+const selectOptions = {
+  ...makeDefaultOptions('select'),
+  string: () => selectToNameString,
+  stringEnum: <T extends string>(values: T[]): NotionPropertyDefinition<'select', T> => {
+    return {
+      type: 'select',
+      handler: (value: ValueType<'select'>): T => {
+        const name = value.select?.name;
+        if (!name || !values.includes(name as T)) {
+          throw Error('Invalid status');
+        }
+        return name as T;
+      }
+    }
+  },
+  optionalStringEnum: <T extends string>(values: T[]): NotionPropertyDefinition<'status', T | undefined> => {
+    return {
+      type: 'status',
+      handler: (value: ValueType<'status'>): T | undefined => {
+        const name = value.status?.name;
+        if (!name) {
+          return undefined;
+        }
+        if (!values.includes(name as T)) {
+          throw Error('Invalid status');
+        }
+        return name as T;
+      }
+    }
+  }
+}
+export function select() {
+  return selectOptions;
+}
+
+const statusToNameString: NotionMutablePropertyDefinition<'status', string> = {
+  type: 'status',
+  handler: value => value.status?.name ?? '',
+  composer: (value) => ({ name: value })
+}
+const statusOptions = {
+  ...makeDefaultOptions('status'),
+  string: () => statusToNameString,
+  stringEnum: <T extends string>(values: readonly T[]): NotionPropertyDefinition<'status', T> => {
+    return {
+      type: 'status',
+      handler: (value: ValueType<'status'>): T => {
+        const name = value.status?.name;
+        if (!name || !values.includes(name as T)) {
+          throw Error('Invalid status: ' + name);
+        }
+        return name as T;
+      }
+    }
+  },
+}
+export function status() {
+  return statusOptions;
+}
+
+const titleToPlainText: NotionMutablePropertyDefinition<'title', string> = {
+  type: 'title',
+  handler: value => packPlainText(value.title),
+  composer: (value) => [{ text: { content: value } }]
+}
+const titleOptions = {
+  ...makeDefaultOptions('title'),
+  plainText: () => titleToPlainText
+}
+export function title() {
+  return titleOptions;
+}
+
+const urlToString: NotionMutablePropertyDefinition<'url', string> = {
+  type: 'url',
+  handler: value => value.url ?? '',
+  composer: (value) => value
+}
+const urlOptions = {
+  ...makeDefaultOptions('url'),
+  string: () => urlToString
+}
+export function url() {
+  return urlOptions;
 }
